8d9590863e9ec1b9b895ccaa720ef514dc25778a
Jappie Klooster <jappieklooster@hotmail.com>
2015-02-10 13:17:20 +0800

    removed dependecy

--- src/main/groovy/nl/jappieklooster/ISAD2/points/rendering/Window.java
+++ src/main/groovy/nl/jappieklooster/ISAD2/points/rendering/Window.java
@@ -1,22 +1,17 @@
 package nl.jappieklooster.ISAD2.points.rendering;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import net.miginfocom.swing.MigLayout;
-import nl.jappieklooster.ISAD2.points.strategies.BruteClosePointSolver;
-import nl.jappieklooster.ISAD2.points.strategies.DivideAndConquerPointSolver;
-
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-import javax.swing.JTextField;
-import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import nl.jappieklooster.ISAD2.points.strategies.BruteClosePointSolver;
+import nl.jappieklooster.ISAD2.points.strategies.DivideAndConquerPointSolver;
 
 public class Window extends JFrame {
 
@@ -35,36 +30,36 @@ public class Window extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		graphPanel = new GraphPanel();
 		graphPanel.setBounds(17, 25, 890, 749);
 		contentPane.add(graphPanel);
-		
-		JButton btnGenerateBrute = new JButton("Generate and brute find");
+
+		final JButton btnGenerateBrute = new JButton("Generate and brute find");
 		btnGenerateBrute.setBounds(226, 786, 206, 25);
 		btnGenerateBrute.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(final MouseEvent e) {
 				graphPanel.getArtist().setSolver(new BruteClosePointSolver());
 				redraw();
 			}
 		});
 		contentPane.add(btnGenerateBrute);
-		
+
 		textFieldAmount = new JTextField();
 		textFieldAmount.setText(Integer.toString(defaultPointAmount));
 		textFieldAmount.setBounds(97, 789, 74, 19);
 		contentPane.add(textFieldAmount);
 		textFieldAmount.setColumns(10);
-		
-		JLabel lblAmount = new JLabel("Amount:");
+
+		final JLabel lblAmount = new JLabel("Amount:");
 		lblAmount.setBounds(17, 791, 70, 15);
 		contentPane.add(lblAmount);
-		
-		JButton btnGenerateAndDevide = new JButton("Generate and devide find");
+
+		final JButton btnGenerateAndDevide = new JButton("Generate and devide find");
 		btnGenerateAndDevide.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(final MouseEvent e) {
 
 				graphPanel.getArtist().setSolver(new DivideAndConquerPointSolver());
 				redraw();
@@ -74,12 +69,12 @@ public class Window extends JFrame {
 		contentPane.add(btnGenerateAndDevide);
 	}
 	@Override
-	public void setVisible(boolean to){
+	public void setVisible(final boolean to){
 		super.setVisible(to);
 		graphPanel.makeGraph(defaultPointAmount);
-		
+
 	}
-	
+
 	protected void redraw(){
 		graphPanel.makeGraph(Integer.parseInt(textFieldAmount.getText()));
 		this.repaint();
